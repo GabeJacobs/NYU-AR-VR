ab195e4b133158f68a6dfa20dcadbf67ac2a4276
GabeJacobs <GabeJacobs824@gmail.com>
2021-05-24 12:02:36 -0400

    Ui buttons

---
+++ Week7-UI/UI-Testing/Assets/Scripts/GameSelect/Example.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Example : MonoBehaviour
+{
+
+    public string title;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<TextMeshProUGUI>().text = title;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
