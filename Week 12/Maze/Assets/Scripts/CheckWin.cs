b02dcf482d52a257f8f6058830570d8b6ad3ea0c
GabeJacobs <GabeJacobs824@gmail.com>
2021-07-06 13:03:28 -0300

    win state

---
+++ Week 12/Maze/Assets/Scripts/CheckWin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckWin : MonoBehaviour
+{
+
+    public GameObject winText;
+    
+    private void Start()
+    {
+        winText.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            if(!winText.activeInHierarchy)
+                winText.SetActive(true);
+        }
+    }
+}
