72953b04aa6755c592e10b32fa50b73614f78406
GabeJacobs <GabeJacobs824@gmail.com>
2021-07-13 16:48:05 -0300

    Slider

---
+++ Week 13/ComplexInteractions/Assets/Scripts/Slider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slider : MonoBehaviour
+{
+    public Transform startPosition;
+    public Transform endPosition;
+
+    private MeshRenderer meshRenderer = null;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void OnSlideStart()
+    {
+        meshRenderer.material.SetColor("_Color", Color.red);
+    }
+    public void OnSlideStop()
+    {
+        meshRenderer.material.SetColor("_Color", Color.white);
+    }
+
+    public void UpdateSlider(float percent)
+    {
+        transform.position = Vector3.Lerp(startPosition.position, endPosition.position, percent);
+    }
+}
