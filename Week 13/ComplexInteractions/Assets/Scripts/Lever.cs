42be4ff644106b1b523d6373f058ef11e93b3d46
GabeJacobs <GabeJacobs824@gmail.com>
2021-07-14 12:57:06 -0300

    Lever

---
+++ Week 13/ComplexInteractions/Assets/Scripts/Lever.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public Transform startOrientation = null;
+    public Transform endOrientation = null;
+
+    private MeshRenderer meshRenderer = null;
+
+    private void Start()
+    {
+        meshRenderer = GetComponentInChildren<MeshRenderer>();
+    }
+
+    public void OnLeverPullStart()
+    {
+        meshRenderer.material.SetColor("_Color", Color.red);
+    }
+    public void OnLeverPullStop()
+    {
+        meshRenderer.material.SetColor("_Color", Color.white);
+    }
+
+    public void UpdateLever(float percent)
+    {
+        transform.rotation = Quaternion.Slerp(startOrientation.rotation, endOrientation.rotation, percent);
+    }
+}
