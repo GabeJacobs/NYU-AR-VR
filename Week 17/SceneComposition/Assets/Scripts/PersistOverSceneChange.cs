52df5fc3ef8bdbfe98254f7787b103bae5d1cd88
gabejacobs824@gmail.com <gabejacobs824@gmail.com>
2021-08-13 13:16:23 -0400

    scene comp draft finished

---
+++ Week 17/SceneComposition/Assets/Scripts/PersistOverSceneChange.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistOverSceneChange : MonoBehaviour
+{
+
+    public bool applyToChildren = true;
+    
+    private int m_persistentLayer = 0;
+    private int m_currentLayer = 0;
+
+    private void Awake()
+    {
+        m_persistentLayer = LayerMask.NameToLayer("XR Persistent");
+        m_currentLayer = gameObject.layer;
+    }
+
+    private void OnEnable()
+    {
+        SceneLoader.Instance.onLoadStart.AddListener(StartPersist);
+        SceneLoader.Instance.onLoadFinish.AddListener(EndPersist);
+    }
+
+    private void OnDisable()
+    {
+        var loader = SceneLoader.Instance;
+        if (loader != null)
+        {
+            SceneLoader.Instance.onLoadStart.RemoveListener(StartPersist);
+            SceneLoader.Instance.onLoadFinish.RemoveListener(EndPersist);
+        }
+    }
+
+    void StartPersist()
+    {
+        m_currentLayer = gameObject.layer;
+        SetLayer(gameObject, m_persistentLayer, applyToChildren);
+    }
+
+    void EndPersist()
+    {
+        SetLayer(gameObject, m_currentLayer, applyToChildren);
+    }
+
+    void SetLayer(GameObject obj, int newLayer, bool applyTochildren)
+    {
+        obj.layer = newLayer;
+        if (applyTochildren)
+        {
+            foreach (Transform child in obj.transform)
+            {
+                SetLayer(child.gameObject, newLayer, applyTochildren);
+            }
+        }
+    }
+    
+}
